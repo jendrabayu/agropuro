a4c74fab90834d66c069b6221cf0307ad7aa6ae1
Jendra Bayu Nugraha <jendra455@gmail.com>
2020-12-20 18:38:04 +0700

    Location Seeder

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -14,30 +14,53 @@ class LocationSeeder extends Seeder
      */
     public function run()
     {
-        $apiKey = env('API_KEY_RAJA_ONGKIR', '206cc2aef17ad58f99012341e0759692');
-        $apiUrl = env('API_URL_RAJA_ONGKIR', 'https://api.rajaongkir.com/starter');
 
-        $provinces = Http::withHeaders([
-            'Key' => $apiKey
-        ])->get($apiUrl . '/province');
+        if (Storage::exists('seeds/provinces.json') && Storage::exists('seeds/cities.json')) {
+            $provinces = collect(json_decode(Storage::get('seeds/provinces.json'), true));
+            $cities = collect(json_decode(Storage::get('seeds/cities.json'), true));
 
-
-        foreach ($provinces['rajaongkir']['results'] as $province) {
-            Province::create([
-                'province_id' => $province['province_id'],
-                'name' => $province['province']
-            ]);
-
-            $cities = Http::withHeaders([
-                'Key' => $apiKey
-            ])->get($apiUrl . '/city?province=' . $province['province_id']);
-
-            foreach ($cities['rajaongkir']['results'] as $city) {
-                City::create([
-                    'province_id' => $city['province_id'],
-                    'city_id' => $city['city_id'],
-                    'name' => $city['city_name']
+            $provinces->each(function ($province) use ($cities) {
+                Province::create([
+                    'province_id' => $province['province_id'],
+                    'name' => $province['province']
                 ]);
+
+                $cities = $cities->where('province_id', $province['province_id']);
+                $cities->each(function ($city) {
+                    City::create([
+                        'province_id' => $city['province_id'],
+                        'city_id' => $city['city_id'],
+                        'name' => $city['city_name']
+                    ]);
+                });
+            });
+        } else {
+
+            $apiKey = env('API_KEY_RAJA_ONGKIR', '206cc2aef17ad58f99012341e0759692');
+            $apiUrl = env('API_URL_RAJA_ONGKIR', 'https://api.rajaongkir.com/starter');
+
+            $provinces = Http::withHeaders([
+                'Key' => $apiKey
+            ])->get($apiUrl . '/province');
+
+
+            foreach ($provinces['rajaongkir']['results'] as $province) {
+                Province::create([
+                    'province_id' => $province['province_id'],
+                    'name' => $province['province']
+                ]);
+
+                $cities = Http::withHeaders([
+                    'Key' => $apiKey
+                ])->get($apiUrl . '/city?province=' . $province['province_id']);
+
+                foreach ($cities['rajaongkir']['results'] as $city) {
+                    City::create([
+                        'province_id' => $city['province_id'],
+                        'city_id' => $city['city_id'],
+                        'name' => $city['city_name']
+                    ]);
+                }
             }
         }
     }
